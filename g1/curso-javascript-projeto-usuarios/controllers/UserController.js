5c4d5014e5c6f48116de363fc7402605712b0cbd
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-02-09 14:53:54 -0300

    Validação de formulario

--- g1/curso-javascript-projeto-usuarios/controllers/UserController.js
+++ g1/curso-javascript-projeto-usuarios/controllers/UserController.js
@@ -10,15 +10,25 @@ class UserController {
     this.formEl.addEventListener('submit', (event) => {
       event.preventDefault();
 
+      let btn = this.formEl.querySelector('[type=submit]');
+
+      btn.disabled = true;
+
       let values = this.getValues();
-      values.photo = '';
+
+      if (!values) return false;
 
       this.getPhoto().then(
         (content) => {
           values.photo = content;
+
           this.addLine(values);
+
+          this.formEl.reset();
+
+          btn.disabled = false;
         },
-        function (e) {
+        (e) => {
           console.error(e);
         },
       );
@@ -28,18 +38,23 @@ class UserController {
   getPhoto() {
     return new Promise((resolve, reject) => {
       let fileReader = new FileReader();
+
       let elements = [...this.formEl.elements].filter((item) => {
         if (item.name === 'photo') {
           return item;
         }
       });
+
       let file = elements[0].files[0];
+
       fileReader.onload = () => {
         resolve(fileReader.result);
       };
-      fileReader.onerror = () => {
+
+      fileReader.onerror = (e) => {
         reject(e);
       };
+
       if (file) {
         fileReader.readAsDataURL(file);
       } else {
@@ -50,8 +65,17 @@ class UserController {
 
   getValues() {
     let user = {};
+    let isValid = true;
 
     [...this.formEl.elements].forEach(function (field, index) {
+      if (
+        ['name', 'email', 'password'].indexOf(field.name) > -1 &&
+        !field.value
+      ) {
+        field.parentElement.classList.add('has-error');
+        isValid = false;
+      }
+
       if (field.name === 'gender') {
         if (field.checked) {
           user[field.name] = field.value;
@@ -62,6 +86,10 @@ class UserController {
         user[field.name] = field.value;
       }
     });
+
+    if (!isValid) {
+      return false;
+    }
 
     return new User(
       user.name,
@@ -77,19 +105,21 @@ class UserController {
 
   addLine(dataUser) {
     let tr = document.createElement('tr');
+
     tr.innerHTML = `
-              <td><img src="${
-                dataUser.photo
-              }" alt="User Image" class="img-circle img-sm"></td>
+          <tr>
+              <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
               <td>${dataUser.name}</td>
               <td>${dataUser.email}</td>
               <td>${dataUser.admin ? 'Sim' : 'Não'}</td>
-              <td>${dataUser.birth}</td>
+              <td>${Utils.dateFormat(dataUser.register)}</td>
               <td>
                   <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                   <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
               </td>
-    `;
+          </tr>
+      `;
+
     this.tableEl.appendChild(tr);
   }
 }
