165d31e1893a635aa0d421f360e72169e414a984
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-02-09 17:14:16 -0300

    Atualizando estatisticas

--- g1/curso-javascript-projeto-usuarios/controllers/UserController.js
+++ g1/curso-javascript-projeto-usuarios/controllers/UserController.js
@@ -106,6 +106,8 @@ class UserController {
   addLine(dataUser) {
     let tr = document.createElement('tr');
 
+    tr.dataset.user = JSON.stringify(dataUser);
+
     tr.innerHTML = `
           <tr>
               <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
@@ -121,5 +123,23 @@ class UserController {
       `;
 
     this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+
+  updateCount() {
+    let numberUsers = 0;
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach((tr) => {
+      numberUsers++;
+
+      let user = JSON.parse(tr.dataset.user);
+
+      if (user._admin) numberAdmin++;
+    });
+
+    document.querySelector('#number-users').innerHTML = numberUsers;
+    document.querySelector('#number-users-admin').innerHTML = numberAdmin;
   }
 }
